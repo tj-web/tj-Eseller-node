169e550c3f4255f12f4f615d36596f7a8aac33bf
Varun Trikha <Varun.Trikha@techjockey.com>
2025-07-10 13:16:31 +0530

    intial step up of the project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
