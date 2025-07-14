05fe176f55726b8a428e9e0dea1d563cb2606544
Varun Trikha <Varun.Trikha@techjockey.com>
2025-07-14 15:35:57 +0530

    adding database

---
+++ src/db/connection.js
@@ -0,0 +1,22 @@
+import { Sequelize } from "sequelize";
+import { configDotenv } from "dotenv";
+configDotenv();
+const sequelize = new Sequelize(
+  process.env.MYSQL_DB,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    host: process.env.MYSQL_HOST,
+    dialect: process.env.MYSQL_DIALECT,
+    logging: false,
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Connected to MySQL database"))
+  .catch((err) =>
+    console.error("Error while connecting to MySQL database. Error:", err)
+  );
+
+export default sequelize;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-const express = require('express');
+import { configDotenv } from "dotenv";
+configDotenv();
+import express from "express";
+import sequelize from "./db/connection.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get("/", (req, res) => {
+  return res.send("Hello, world!");
 });
 
 app.listen(PORT, () => {
