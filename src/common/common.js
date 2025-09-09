46a7bdec32ca542fba592598a249840fc38cbaa7
Varun Trikha <Varun.Trikha@techjockey.com>
2025-09-09 15:50:30 +0530

    review api is completed

--- src/common/common.js
+++ src/common/common.js
@@ -1,36 +1,39 @@
-// // console.log(typeof null);
-// // console.log(null instanceof Object);
-// // console.log([] + []);
-// // console.log([] + {});
-// // console.log({} + []);
-// // console.log([] == ![]);
+export const showAnalytics = true;
 
-// // console.log(Object.prototype);
-// // console.log(Array.prototype);
-
-// // import { spawn }from 'child_process';
-
-// // const ls = spawn('ls', ['-lh', '/usr']); // List files in /usr
-
-// // ls.stdout.on('data', (data) => {
-// //   console.log(`Output: ${data}`);
-// // });
-
-// // ls.stderr.on('data', (data) => {
-// //   console.error(`Error: ${data}`);
-// // });
-
-// // ls.on('close', (code) => {
-// //   console.log(`Process exited with code ${code}`);
-// // });
-// import { exec } from "child_process";
-
-// exec("uname -a", (err, stdout, stderr) => {
-//   if (err) {
-//     console.error("Error:", err);
-//     return;
-//   }
-//   console.log("uname output:", stdout);
-// });
-
-
+export const analytics = {
+  impression: {
+    type: "impression",
+    hasData: true,
+    heading: "Impressions",
+    info: "Number of times your products are shown. An impression is counted each time your product or banner is visible on website.",
+    icon: "/static/images/analytics_icon/app/impression.png", // adjust path
+  },
+  pageviews: {
+    type: "pageviews",
+    hasData: false,
+    heading: "Page Views",
+    info: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+    icon: "/static/images/analytics_icon/app/pageviews.png",
+  },
+  requests: {
+    type: "requests",
+    hasData: true,
+    heading: "Total Requests",
+    info: "Number of potential users who requested phone call or demo for your products.",
+    icon: "/static/images/analytics_icon/app/requests.png",
+  },
+  avg_attemp_time: {
+    type: "avg_attemp_time",
+    hasData: true,
+    heading: "Average Attempt Time",
+    info: "Average time taken by you to connect with potential customer.",
+    icon: "/static/images/analytics_icon/app/avg_attemp_time.png",
+  },
+  total_utilisation: {
+    type: "total_utilisation",
+    hasData: true,
+    heading: "Total Utilisation",
+    info: "% of requests utilized by you. It includes phone call and viewing contact information of a potential customer.",
+    icon: "/static/images/analytics_icon/app/total_utilisation.png",
+  },
+};
