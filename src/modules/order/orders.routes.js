6ae0b15d8d481c57af05c6f3b47cf0dc30d0bcf8
theanandtripathi1 <anand.tripathi@techjockey.com>
2026-04-16 13:14:31 +0530

    modified order module to fetch data , and return total count of orders as well !

--- src/modules/order/orders.routes.js
+++ src/modules/order/orders.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { getOrders } from "./orders.controller.js";
 const router = express.Router();
- 
-router.get("/",getOrders);
- 
+
+router.get("/getOrders",getOrders);
+
 export default router;
