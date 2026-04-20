e75509e13c7dde1f5dd61cc13c3ea3d19887b881
Shiv Prakash <shivprakash@MacBook-Air.local>
2026-04-20 16:32:23 +0530

    Frontnd url updated

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5001", "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   }),
 );
