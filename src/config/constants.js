7364a87f370f0fc010c7ef7ef7b4c4a4ad2125b3
Shiv Prakash <shivprakash@MacBook-Air.local>
2026-04-20 17:47:49 +0530

    files base path modified

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,12 +1,12 @@
 export const AWS_paths = () => {
   //Base URL
   const AWS_PATH = process.env.AWS_PATH;
-  const AWS_PATH_WEB = `${AWS_PATH}/web`;
-    const AWS_UPLOAD_WEB = "web";
+  const AWS_PATH_WEB = `${AWS_PATH}web`;
+    const AWS_UPLOAD_WEB = "/web";
     const AWS_BRAND_IMAGES = `${AWS_UPLOAD_WEB}/assets/images/techjockey/brands/}`
   /* upload and fetch path of eseller and web and manage */
   const AWS_FETCH_PRODUCT_IMAGES = `${AWS_PATH_WEB}/assets/images/techjockey/products/`;
-  const DIR_FS_PRODUCT_NOIMAGE = `${AWS_PATH}/assets/images/techjockey/no-image.png`;
+  const DIR_FS_PRODUCT_NOIMAGE = `${AWS_PATH}assets/images/techjockey/no-image.png`;
   return {
     AWS_PATH,
     AWS_PATH_WEB,
