43449a9717ea589ee4996390556eff4aee03ace5
Shiv Prakash <shivprakash@MacBook-Air.local>
2026-05-18 18:10:57 +0530

    checking lead insights

--- src/db/mongo.js
+++ src/db/mongo.js
@@ -5,7 +5,8 @@ configDotenv();
 
 const connectMongo = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGO_CONNECTION_STRING}`, {
+        console.log(`mongo string`, process.env.MONGO_CONNECTION_STRING);
+        await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
