58af56cd156b1b01f4fbc1bd426f6df6902fef29
Shiv Prakash <shivprakash@MacBook-Air.local>
2026-05-19 11:28:44 +0530

    Apollo API Endpoint changed

--- src/modules/lead/manageLeads.service.js
+++ src/modules/lead/manageLeads.service.js
@@ -942,7 +942,7 @@ const getOrganizationData = async (domain) => {
         return { status: 0, msg: "API Key missing" };
     }
 
-    const url = `https://api.apollo.io/api/v1/organizations/enrich?domain=${encodeURIComponent(domain)}`;
+    const url = `${process.env.APOLLO_API_URL}organizations/enrich?domain=${encodeURIComponent(domain)}`;
 
     try {
         const response = await fetch(url, {
@@ -1072,9 +1072,8 @@ const employeeData = async (domain, queryString) => {
     };
 
     let resArr = [];
-    const urlWithQuery = `https://api.apollo.io/api/v1/mixed_people/search?${queryString}q_organization_domains_list[]=${encodeURIComponent(domain)}`;
-    //    const urlWithQuery =
-    // `https://api.apollo.io/api/v1/mixed_people/api_search?${queryString}q_organization_domains_list[]=${encodeURIComponent(domain)}&page=1&per_page=5`;
+    // const urlWithQuery = `https://api.apollo.io/api/v1/mixed_people/search?${queryString}q_organization_domains_list[]=${encodeURIComponent(domain)}`;
+       const urlWithQuery = `${process.env.APOLLO_API_URL}mixed_people/api_search?${queryString}q_organization_domains_list[]=${encodeURIComponent(domain)}&page=1&per_page=5`;
     try {
         const response = await fetchWithCurl(urlWithQuery, headers);
         const data = await response.json();
@@ -1091,9 +1090,8 @@ const employeeData = async (domain, queryString) => {
 
         if (resArr.length < 5) {
             const remainLen = 5 - resArr.length;
-            const urlWithoutQuery = `https://api.apollo.io/api/v1/mixed_people/search?q_organization_domains_list[]=${encodeURIComponent(domain)}`;
-            // const urlWithoutQuery =
-            // `https://api.apollo.io/api/v1/mixed_people/api_search?q_organization_domains_list[]=${encodeURIComponent(domain)}&page=1&per_page=5`;
+            // const urlWithoutQuery = `https://api.apollo.io/api/v1/mixed_people/search?q_organization_domains_list[]=${encodeURIComponent(domain)}`;
+            const urlWithoutQuery = `${process.env.APOLLO_API_URL}v1/mixed_people/api_search?q_organization_domains_list[]=${encodeURIComponent(domain)}&page=1&per_page=5`;
             const responseNoQuery = await fetchWithCurl(urlWithoutQuery, headers);
             const dataNoQuery = await responseNoQuery.json();
             const morePeople = (dataNoQuery.people || []).slice(0, remainLen);
